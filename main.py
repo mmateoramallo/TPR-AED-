c6b3c9b44318c8bb76403702b3da7fdec4946fc0
Mateo <mateo.j.ramallo@gmail.com>
2022-10-26 16:06:38 -0300

    Procesar Texto

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-#Importo librerias
+# Importo librerias
 import random
 from Series import Serie
+import os.path
 
-#1) Procesar el archivo de texto generos.txt para crear un vector que contenga los nombres de los mismos (Únicamente los nombres y en el mismo orden en el que se encuentran en el archivo).
-def process_txt():
-    #Debemos abrir el archivo
-    m = open()
 
+# 1) Procesar el archivo de texto generos.txt para crear un vector que contenga los nombres de los mismos (Únicamente los nombres y en el mismo orden en el que se encuentran en el archivo).
+def process_txt(vec):
+    # Debemos abrir el archivo
+    m = open("generos.txt", mode='rt', encoding="utf8")
+    for linea in m:
+        if not (linea == '\n'):
+            vec.append(linea)
 
 
 def main():
-    pass
-    #Generamos el vector
+    # Generamos el vector con los generos de las series
+    vec_gen = []
+    process_txt(vec_gen)
+    print(vec_gen)
 
 
 if __name__ == '__main__':
