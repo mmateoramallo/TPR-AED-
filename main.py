161ce086956146f2a34515bfda703ed21e80bb1f
Mateo <mateo.j.ramallo@gmail.com>
2022-10-26 19:35:44 -0300

    Primer Punto Hecho, en camino numero 2

--- main.py
+++ main.py
@@ -22,14 +22,33 @@ def process_txt(vec):
 
 def cargar_registros(vec_regs):
     #Primero abrimos el archivo de series_aed
-    m = open("series_aed.csv", mode='rt', encoding='utf8')
+    m = open("series_aed.csv", mode='rt')
+    #Establecemos un contadar, para validar que no se procese el encabezado
+    c = 0
+    #Recorremos el archivo
+    for linea in m:
+        if c > 0:
+            #En este punto ya estamos procesando las lineas del archivo, almacenamos la linea como un array, separando las palabras
+            txt_line = linea.split('|')
+            print(txt_line)
+
+
+        c += 1
+
 
 
 def main():
     # Generamos el vector con los generos de las series
     vec_gen = []
+    #Generamos el vector de registros
+    vec_regs = []
+
+
     process_txt(vec_gen)
     print(vec_gen)
+
+    cargar_registros(vec_regs)
+
 
 
 if __name__ == '__main__':
