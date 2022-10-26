f19650d5f2ada88eaa67b74fd548a1d0124558cb
Mateo <mateo.j.ramallo@gmail.com>
2022-10-26 15:52:41 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Importo librerias
+import random
+from Series import Serie
+
+#1) Procesar el archivo de texto generos.txt para crear un vector que contenga los nombres de los mismos (Únicamente los nombres y en el mismo orden en el que se encuentran en el archivo).
+def process_txt():
+    #Debemos abrir el archivo
+    m = open()
+
+
+
+def main():
+    pass
+    #Generamos el vector
+
+
+if __name__ == '__main__':
+    main()
