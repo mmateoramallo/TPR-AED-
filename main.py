29abdd8f8b84581752d24e20e54c4d5690024d8f
Mateo <mateo.j.ramallo@gmail.com>
2022-10-28 01:31:15 -0300

    Tengo que arreglar la visualizacion de la 4 pero con una muestra de 5 objetos funciona correctamente

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os.path
 
 # Menu
 def mostrar_menu():
+    print()
     print('Bienvenido al Sistema de Gestión de Series: ')
     print('1- CARGUE EN UN VECTOR LOS TIPOS DE GÉNEROS DE SERIES. ')
     print('2- GENERAR VECTOR DE REGISTROS DE SERIES. ')
@@ -15,6 +16,7 @@ def mostrar_menu():
     print('6.- MOSTRAR ARCHIVO BINARIO DE REGISTROS.')
     print('7.- MOSTRAR SI EXISTE SERIE DE TÍTULO \'TIT\'. SI ESTÁ AUMENTAR EN 1 VOTO. SINO, MOSTRAR MENSAJE.')
     print('0.- SALIR DEL PROGRAMA.')
+    print()
 
 
 # 1) Procesar el archivo de texto generos.txt para crear un vector que contenga los nombres de los mismos (Únicamente los nombres y en el mismo orden en el que se encuentran en el archivo).
@@ -35,7 +37,26 @@ def process_txt(vec):
 
 
 # 2) Procesar el archivo de texto series.csv para generar un vector de registros de series con el siguiente formato:
-def cargar_registros(vec_regs):
+
+def insercion_binaria_por_numero_de_votos(vector_registros, registro):
+    n = len(vector_registros)
+    izq, der = 0, n - 1
+    pos = n
+
+    while izq <= der:
+        c = (izq + der) // 2
+        if registro.No_of_Vote == vector_registros[c].No_of_Vote:
+            pos = c
+            break
+        if registro.No_of_Vote > vector_registros[c].No_of_Vote:
+            der = c - 1
+        else:
+            izq = c + 1
+    if izq > der:
+        vector_registros[pos:pos] = [registro]
+
+
+def cargar_registros(vec_regs, vec_gen):
     # Primero abrimos el archivo de series_aed
     m = open("series_aed.csv", mode='rt')
     # Establecemos un contadar, para validar que no se procese el encabezado
@@ -54,12 +75,28 @@ def cargar_registros(vec_regs):
             Genre = txt_line[5]
             IMDB_Rating = txt_line[6]
             Overwiew = txt_line[7]
+            No_of_Vote = txt_line[12]
+            indice = 0
+            #Verifico que tenga duracion
+            if Runtime_of_Episodes != '':
+                #Eliminamos el min
+                for i in range(len(Runtime_of_Episodes)):
+                    if Runtime_of_Episodes[i] == ' ':
+                        indice = i
+                Runtime_of_Episodes = Runtime_of_Episodes[:indice]
+                #Obtengo el codigo del genero
+                for i in range(len(vec_gen)):
+                    if Genre == vec_gen[i]:
+                        Genre = i
+                serie = Serie(Poster_Link, Series_Title, Runtime_of_Series, Certificate, Runtime_of_Episodes, Genre, IMDB_Rating, Overwiew, No_of_Vote)
 
-            # Instanciamos la clase
-            serie = Serie(Poster_Link, Series_Title, Runtime_of_Series, Certificate, Runtime_of_Episodes, Genre, IMDB_Rating, Overwiew, None)
-            #Agregamos al vector
-            vec_regs.append(serie)
+                #Agrego los elementos
+                insercion_binaria_por_numero_de_votos(vec_regs, serie)
         c += 1
+
+
+    #Cerrar el archivo
+    m.close()
 
 
 def main():
@@ -76,10 +113,11 @@ def main():
         if op == 1:
             print('-' * 21, 'Procesar Archivo de Texto', '-' * 21)
             process_txt(vec_gen)
+            print(vec_gen)
         elif op == 2:
             print('-' * 21, 'CARGANDO VECTOR DE REGISTROS', '-' * 21)
-            cargar_registros(vec_regs)
-            #Recorremos el vector para mostrar las series que se cargaron en el mismo
+            cargar_registros(vec_regs, vec_gen)
+            # Recorremos el vector para mostrar las series que se cargaron en el mismo
             for i in vec_regs:
                 print(i)
         elif op == 3:
